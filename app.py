d6d21ccc35707ee2a3480cd8fb5602eaa1b25569
samlukas <samuelplukas@gmail.com>
2023-01-21 15:55:30 -0500

    Cohere functionality and sockets work

--- app.py
+++ app.py
@@ -1,25 +1,70 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_socketio import SocketIO
+import requests
+import cohere
+from cohere.classify import Example
+
+# Custom API key with toxic message board dataset
+co = cohere.Client('3E7E98dyvUvoVm59NG3Z9HWyTuUaKdvNOaxx7u8r')
+
+# Add more examples to narrow down on exceptions (double negatives etc...)
+examples = [
+  Example("you are hot trash", "Toxic"),  
+  Example("go to hell", "Toxic"),
+  Example("get rekt moron", "Toxic"),  
+  Example("get a brain and use it", "Toxic"), 
+  Example("say what you mean, you jerk.", "Toxic"), 
+  Example("Are you really this stupid", "Toxic"), 
+  Example("I will honestly kill you", "Toxic"),  
+  Example("yo how are you", "Benign"),  
+  Example("I'm curious, how did that happen", "Benign"),  
+  Example("Try that again", "Benign"),  
+  Example("Hello everyone, excited to be here", "Benign"), 
+  Example("I think I saw it first", "Benign"),  
+  Example("That is an interesting point", "Benign"), 
+  Example("I love this", "Benign"), 
+  Example("We should try that sometime", "Benign"), 
+  Example("You should go for it", "Benign")
+]
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+app.config['SECRET'] = "test12345!"
 socketio = SocketIO(app)
-
+url = 'http://127.0.0.1:5000'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template("index.html")
 
 def messageReceived(methods=['GET', 'POST']):
     print('Message was received')
 
+def messageError(methods=['GET', 'POST']):
+    print('Message error: Toxic')
+
+def is_toxic(msg):
+    response = co.classify(
+        model='large',
+        inputs=[msg],
+        examples=examples
+        )
+
+    if response[0].prediction == 'Toxic' and response[0].confidence >= 0.8:
+        return True
+    else:
+        return False
+
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+    if 'data' in json:
+        pass
+    elif is_toxic(json['message']) is True:
+        socketio.emit('my response', {'message': 'Toxic'}, callback=messageError)
+    else:
+        socketio.emit('my response', json, callback=messageReceived)
 
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+if __name__ == "__main__":
+    app.run()
+    socketio.run(app, host="localhost")
