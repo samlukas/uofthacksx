9ce28493f58a73357550b3ad7e34f232cd324e5e
samlukas <samuelplukas@gmail.com>
2023-01-21 21:29:04 -0500

    Revert "Added SQL database, topics appear on the side"
    This reverts commit 4c4813d62ea46ae448ac85d14c1738284dc93f05.

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_socketio import SocketIO
 import requests
 import cohere
 from cohere.classify import Example
-import sqlite3
 
 # Custom API key with toxic message board dataset
 co = cohere.Client('3E7E98dyvUvoVm59NG3Z9HWyTuUaKdvNOaxx7u8r')
@@ -36,28 +35,8 @@ app.config['SECRET'] = "test12345!"
 socketio = SocketIO(app)
 url = 'http://127.0.0.1:5000'
 
-
-def db_connection():
-    conn = sqlite3.connect('topics.db')
-    
-    with open('topic.sql') as f:
-        conn.executescript(f.read())
-
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
 @app.route('/')
 def index():
-    conn = db_connection()
-    cur = conn.cursor()
-    count = cur.execute("SELECT COUNT(*) FROM topics")
-
-    if count != 0:
-        topics = conn.execute("SELECT * FROM topics").fetchall()
-        conn.close()
-        return render_template("index.html", topics=topics)
-
     return render_template("index.html")
 
 def messageReceived(methods=['GET', 'POST']):
@@ -88,20 +67,6 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     else:
         socketio.emit('my response', json, callback=messageReceived)
 
-
-@app.route('/newTopic', methods=["POST"])
-def newTopic():
-    topic = request.form.get("topic")
-
-    conn = db_connection()
-    cur = conn.cursor()
-    cur.execute("INSERT INTO topics (topic, description) VALUES(?, ?)", (topic, topic))
-
-    conn.commit()
-    conn.close()
-
-    return redirect("/")
-    
 
 if __name__ == "__main__":
     app.run()
--- topic.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS topics (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    topic TEXT NOT NULL,
-    description TEXT NOT NULL
-);
